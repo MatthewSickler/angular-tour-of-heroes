8438ea382d2d7a592b10e3ef82cb466fa3e4a905
MatthewSickler <matthew.sickler@indyimaging.com>
2017-04-27 13:31:42 -0400

    Step 5 Partial: Added routing to hero list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -8,7 +10,13 @@ import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,
+    RouterModule.forRoot([
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    }
+  ])],
   bootstrap: [AppComponent],
   providers: [HeroService]
 })
