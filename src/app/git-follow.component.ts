8e0f061075da49408273d3f1b8b3e750b9b2139c
MatthewSickler <matthew.sickler@indyimaging.com>
2017-05-23 13:13:38 -0400

    clean up rx tutorial code and added disabling buttons when query is active

--- src/app/git-follow.component.ts
+++ src/app/git-follow.component.ts
@@ -14,14 +14,14 @@ export class GitFollowComponent implements OnInit {
   gitURLOffset: string = 'https://api.github.com/users?since=';
   userList: any[] = [];
   userListShow: any[] = [];
+  disableButtons: boolean = false;
 
-  //Called on button clicks
   refreshClickObs: Subject<any> = new Subject();
   clearUserClickObs: Subject<number> = new Subject();
 
 
   requestObs: Observable<any> = this.refreshClickObs.startWith('start').map(() => {
-    this.userListShow = []
+    this.disableButtons = true;
     let randomOffset = Math.floor(Math.random()*500);
     return this.gitURLOffset + randomOffset;
   });
@@ -29,15 +29,15 @@ export class GitFollowComponent implements OnInit {
   responseObs: Observable<any> = this.requestObs.flatMap((url: string) => this.gitFollowService.getQueryFromURL(url));
 
   newUserObs: Observable<any> = this.clearUserClickObs.startWith(-1).combineLatest(this.responseObs, (index, users) => {
-    // let usersJSON = users.json();
-    if(this.userListShow.length === 0) {
+    if(this.disableButtons) {
       this.userListShow[0] = users[Math.floor(Math.random()*users.length)];
       this.userListShow[1] = users[Math.floor(Math.random()*users.length)];
       this.userListShow[2] = users[Math.floor(Math.random()*users.length)];
+
+      this.disableButtons = false;
     } else {
       this.userListShow[index] = users[Math.floor(Math.random()*users.length)];
     }
-
   });
 
 
